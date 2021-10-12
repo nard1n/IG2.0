0777ab660b0fc21b59cd7693a0518094a3846fd9
Nardin Lachowski <nardin@live.com>
2021-10-12 12:24:33 -0400

    started the header component, logo and search bar done

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <div className="flex justify-between max-w-6xl">
+                {/* left */}
+                <div className="relative hidden lg:inline-grid w-24 flex-shrink-0 cursor-pointer">
+                    <Image
+                        src="https://links.papareact.com/ocw"
+                        layout="fill"
+                        objectFit="contain"
+                    />
+                </div>
+                <div className="relative lg:hidden w-10 cursor-pointer">
+                    <Image
+                        src="https://links.papareact.com/jjm"
+                        layout="fill"
+                        objectFit="contain"
+                    />
+                </div>
+
+                {/* middle - search input field */}
+                <div className="relative mt-1 p-3 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <SearchIcon className="h-5 w-5 text-gray-500" />
+                    </div>
+                    <input type="text" placeholder="Search" className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" />
+                </div>
+
+                {/* right */}
+            </div>
+
         </div>
     )
 }
