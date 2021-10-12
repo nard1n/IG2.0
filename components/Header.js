8c383a7847f5970e77dff2b9c07118a90fabd8d2
Nardin Lachowski <nardin@live.com>
2021-10-12 12:49:40 -0400

    added the right side header icons with styling

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,13 @@ import {
     PaperAirplaneIcon,
     MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
     return (
         <div>
-            <div className="flex justify-between max-w-6xl">
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+
                 {/* left */}
                 <div className="relative hidden lg:inline-grid w-24 flex-shrink-0 cursor-pointer">
                     <Image
@@ -30,14 +32,39 @@ const Header = () => {
                 </div>
 
                 {/* middle - search input field */}
-                <div className="relative mt-1 p-3 rounded-md">
-                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                        <SearchIcon className="h-5 w-5 text-gray-500" />
+                <div className="max-w-xs">
+                    <div className="relative mt-1 p-3 rounded-md">
+                        <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                            <SearchIcon className="h-5 w-5 text-gray-500" />
+                        </div>
+                        <input type="text" placeholder="Search" className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" />
                     </div>
-                    <input type="text" placeholder="Search" className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" />
                 </div>
 
                 {/* right */}
+                
+                <div className="flex items-center justify-end space-x-4">
+                    <HomeIcon className="navBtn" />
+
+                    <MenuIcon className="h-6 md:hidden cursor-pointer" />
+
+                    <div className="relative navBtn">
+                        <PaperAirplaneIcon className="navBtn rotate-45" />
+                        <div className="absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+                    </div>
+                    <PlusCircleIcon className="navBtn" />
+                    <UserGroupIcon className="navBtn" />
+                    <HeartIcon className="navBtn" />
+
+                    <img
+                        src="https://links.papareact.com/3ke"
+                        alt="profile pic"
+                        className="h-10 rounded-full cursor-pointer"
+                    />
+                    
+
+                </div>
+
             </div>
 
         </div>
