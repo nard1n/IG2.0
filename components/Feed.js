006fce96773b3a0eaf760af7f842eb9de400d8a0
Nardin Lachowski <nardin@live.com>
2021-10-12 13:31:52 -0400

    put together the feed components into which we will import some of the smaller components to display content

---
+++ components/Feed.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Posts from './Posts'
+import Stories from './Stories'
+
+const Feed = () => {
+    return (
+        <div>
+            <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+
+            {/* Left Section */}
+            <section className='col-span-2'>
+                {/* Stories */}
+                <Stories />
+
+                {/* Posts */}
+                <Posts />
+            </section>
+            
+
+            {/* Right Section */}
+            <section>
+                {/* mini profile */}
+                {/* Suggestions */}
+            </section>
+
+            </main>
+        </div>
+    )
+}
+
+export default Feed
