be02569fe88f334912ccab3f1baaa48005d9af01
Nardin Lachowski <nardin@live.com>
2021-10-12 13:28:13 -0400

    importing feed component, added bg color and scrollbar styling to app

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram 2.0 Build</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      {/* Feed */}
+      <Feed />
       {/* Modal */}
     </div>
   )
