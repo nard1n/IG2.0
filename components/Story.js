cecc2844da78e4d6889d2b7267c66101fb2d578d
Nardin Lachowski <nardin@live.com>
2021-10-12 13:30:26 -0400

    added styling to the profiles displaying in the stories

---
+++ components/Story.js
@@ -0,0 +1,10 @@
+function Story({ img, username }) {
+    return (
+        <div>
+            <img className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform duration-200 ease-out" src={img} alt={username} />
+            <p className="text-xs w-14 truncate text-center">{username}</p>
+        </div>
+    )
+}
+
+export default Story
