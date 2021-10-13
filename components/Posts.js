cf247fb4ffc8954ff2618891673e864bfe35a509
Nardin Lachowski <nardin@live.com>
2021-10-13 09:59:29 -0400

    mapping over posts data and passing the data of each to the Post compp for rendering, data temporarily random will replace with db later

--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import Post from './Post'
+
+const posts = [
+    {
+        id: '123',
+        username: 'nard1n',
+        userImg: 'https://links.papareact.com/3ke',
+        img: 'https://links.papareact.com/3ke',
+        caption: 'This is dope',
+    },
+    {
+        id: '234',
+        username: 'nardoon',
+        userImg: 'https://links.papareact.com/3ke',
+        img: 'https://links.papareact.com/3ke',
+        caption: 'This is dope',
+    },
+]
 
 const Posts = () => {
     return (
         <div>
-            
+            {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    username={post.username}
+                    userImg={post.userImg}
+                    img={post.img}
+                    caption={post.caption} />
+            ))}
         </div>
     )
 }
