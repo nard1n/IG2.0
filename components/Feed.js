ec8b1419027b066e343ac0e22fee32d92343fdb9
Nardin Lachowski <nardin@live.com>
2021-10-13 09:56:11 -0400

    importing Stories and Posts components

--- components/Feed.js
+++ components/Feed.js
@@ -5,14 +5,11 @@ import Stories from './Stories'
 const Feed = () => {
     return (
         <div>
-            <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+            <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
 
             {/* Left Section */}
             <section className='col-span-2'>
-                {/* Stories */}
                 <Stories />
-
-                {/* Posts */}
                 <Posts />
             </section>
             
