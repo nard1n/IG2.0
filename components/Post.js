9c2c2b5b0cc9e297ede4236e139873f874318772
Nardin Lachowski <nardin@live.com>
2021-10-13 09:57:12 -0400

    start of the Post component - caption, comments, input box next

---
+++ components/Post.js
@@ -0,0 +1,47 @@
+import {
+    BookmarkIcon,
+    ChatIcon,
+    DotsHorizontalIcon,
+    EmojiHappyIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled} from "@heroicons/react/solid"
+
+function Post({ id, username, userImg, img, caption }) {
+    return (
+        <div className="bg-white my-7 border rounded-sm">
+            
+            <div className=" flex items-center p-5">
+                <img 
+                    src={userImg}
+                    alt={username}
+                    className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+                />
+                <p className="flex-1 font-bold">{username}</p>
+                <DotsHorizontalIcon className="h-5" />
+            </div>
+            
+            <img
+                src={img}
+                alt=""
+                className="object-cover w-full"
+            />
+            <div className="flex justify-between px-4 pt-4">
+                <div className="flex space-x-4">
+                    <HeartIcon className="actionBtn" />
+                    <ChatIcon className="actionBtn" />
+                    <PaperAirplaneIcon className="actionBtn rotate-45" />
+
+                </div>
+                <BookmarkIcon className="actionBtn" />
+            </div>
+
+            
+            {/*  caption, comments, input box */}
+
+        </div>
+    )
+}
+
+export default Post
