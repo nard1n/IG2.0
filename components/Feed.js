d9e301447ae061718cc1032701cdcfcf7829620f
Nardin Lachowski <nardin@live.com>
2021-10-18 15:11:45 -0400

    importing new right side components into Feed view, added some styling too

--- components/Feed.js
+++ components/Feed.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
+import Suggestions from './Suggestions'
 
 const Feed = () => {
     return (
         <div>
             <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
 
-            {/* Left Section */}
-            <section className='col-span-2'>
-                <Stories />
-                <Posts />
-            </section>
-            
-
-            {/* Right Section */}
-            <section>
-                {/* mini profile */}
-                {/* Suggestions */}
-            </section>
+                {/* Left Section */}
+                <section className='col-span-2'>
+                    <Stories />
+                    <Posts />
+                </section>
+                
+                {/* Right Section */}
+                <section className="hidden xl:inline-grid md:col-span-1">
+                    <div className="fixed top-20">
+                        <MiniProfile />
+                        <Suggestions />
+                    </div>
+                </section>
 
             </main>
         </div>
