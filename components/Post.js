1982896dbec907bf591e63f7dc82ca06518ba45e
Nardin Lachowski <nardin@live.com>
2021-10-18 14:52:02 -0400

    added the caption to post as well as the comment box to display

--- components/Post.js
+++ components/Post.js
@@ -37,8 +37,24 @@ function Post({ id, username, userImg, img, caption }) {
                 <BookmarkIcon className="actionBtn" />
             </div>
 
-            
-            {/*  caption, comments, input box */}
+            {/* caption */}
+            <p className="p-5 truncate">
+                <span className="font-bold mr-1">{username} </span>
+                {caption}
+            </p>
+
+            {/* comments */}
+
+            {/* input box */}
+            <form className="flex items-center p-4">
+                <EmojiHappyIcon className="h-7" />
+                <input 
+                    type="text" 
+                    className="border-none flex-1 focus:ring-0 outline-none"
+                    placeholder="Add a comment... "
+                />
+                <button className="font-semibold text-blue-400">Post</button>
+            </form>
 
         </div>
     )
