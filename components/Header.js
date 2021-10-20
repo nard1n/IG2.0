b69de306934d55d0c6963fff96b80da6b07f3a00
Nardin Lachowski <nardin@live.com>
2021-10-20 19:30:17 -0400

    setting up the session and wrapping the right side suggestions with it to only allow them when user logs in

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,13 @@ import {
     MenuIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Header = () => {
+    const { data: session } = useSession();
+
+    console.log(session)
+
     return (
         <div className="shadow-sm border-sm bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -45,23 +50,29 @@ const Header = () => {
                 
                 <div className="flex items-center justify-end space-x-4">
                     <HomeIcon className="navBtn" />
-
                     <MenuIcon className="h-6 md:hidden cursor-pointer" />
 
-                    <div className="relative navBtn">
-                        <PaperAirplaneIcon className="navBtn rotate-45" />
-                        <div className="absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
-                    </div>
-                    <PlusCircleIcon className="navBtn" />
-                    <UserGroupIcon className="navBtn" />
-                    <HeartIcon className="navBtn" />
+                    {session ? (
+                        <>
+                            <div className="relative navBtn">
+                                <PaperAirplaneIcon className="navBtn rotate-45" />
+                                <div className="absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+                            </div>
+                            <PlusCircleIcon className="navBtn" />
+                            <UserGroupIcon className="navBtn" />
+                            <HeartIcon className="navBtn" />
 
-                    <img
-                        src="https://links.papareact.com/3ke"
-                        alt="profile pic"
-                        className="h-10 rounded-full cursor-pointer"
-                    />
-                    
+                            <img
+                                onClick={signOut}
+                                src={session?.user?.image}
+                                alt=""
+                                className="h-10 w-10 rounded-full cursor-pointer"
+                            />
+                        </>
+                        ) : (
+                            <button onClick={signIn}>Sign In</button>
+                        )
+                    }
 
                 </div>
 
