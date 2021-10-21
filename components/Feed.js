3dcd84eb9349c422e9abfe193d4776137f232f6f
Nardin Lachowski <nardin@live.com>
2021-10-21 10:31:18 -0400

    updating the information displayed based on the login status of the session

--- components/Feed.js
+++ components/Feed.js
@@ -3,11 +3,14 @@ import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
 import Suggestions from './Suggestions'
+import { useSession } from "next-auth/react";
 
 const Feed = () => {
+    const { data: session } = useSession();
+
     return (
         <div>
-            <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+            <main className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
 
                 {/* Left Section */}
                 <section className='col-span-2'>
@@ -16,12 +19,15 @@ const Feed = () => {
                 </section>
                 
                 {/* Right Section */}
-                <section className="hidden xl:inline-grid md:col-span-1">
-                    <div className="fixed top-20">
-                        <MiniProfile />
-                        <Suggestions />
-                    </div>
-                </section>
+                {session && (
+                    <section className="hidden xl:inline-grid md:col-span-1">
+                        <div className="fixed top-20">
+                            <MiniProfile />
+                            <Suggestions />
+                        </div>
+                    </section>
+
+                )}
 
             </main>
         </div>
