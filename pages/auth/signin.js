4ad3f1898e40478aad9b0c1a826570bf726d2684
Nardin Lachowski <nardin@live.com>
2021-10-21 09:45:07 -0400

    fixed up the signin page, adding the header and a callback url to redirect user to dashboard after signin

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,32 @@
 import { getProviders, signIn as signIntoProvider } from "next-auth/react"; 
+import Header from "../../components/Header";
 
 function signIn({ providers }) {
     return (
         <>
-            {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
-                    <button onClick={() => signIntoProvider(provider.id)}>
-                        Sign in with {provider.name}
-                    </button>
+            <Header />
+
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+                <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+
+                <p className="font-xs italic">
+                    This is not a REAL app. This was built for educational purposes only.
+                </p>
+                <div className="mt-40">
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                            <button 
+                                className="p-3 bg-blue-500 rounded-lg text-white"
+                                onClick={() => 
+                                    signIntoProvider(provider.id, {callbackUrl: '/'})
+                                }
+                            >
+                                Sign in with {provider.name}
+                            </button>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </>
     );
 }
