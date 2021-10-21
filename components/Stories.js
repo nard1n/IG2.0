c4c86a42549a2ce4551e298d5eedc7b1b8b7a19f
Nardin Lachowski <nardin@live.com>
2021-10-21 10:59:44 -0400

    adding user session to determine if there should be a logged in uer story circle added

--- components/Stories.js
+++ components/Stories.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import faker from 'faker';
 import Story from './Story';
-
+import { useSession } from "next-auth/react";
 
 function Stories() {
-    const [suggestions, setSuggestions] = useState([])
+    const { data: session } = useSession();
+    const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
         const suggestions = [...Array(20)].map((_, i) => ({
@@ -16,10 +17,15 @@ function Stories() {
 
     return (
         <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+
+            {session && (
+                <Story img={session.user.image} username={session.user.username} />
+            )}
             {suggestions.map(profile => (
                 <Story key={profile.id} img={profile.avatar} username={profile.username} />
             ))}
             
+
         </div>
     )
 }
