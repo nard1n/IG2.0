a5ed53285b9fcf654598d8cf41c7015c34c9f706
Nardin Lachowski <nardin@live.com>
2022-02-26 18:22:19 -0500

    importing the modal component into the app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
 
       <Header />
       <Feed />
-      {/* Modal */}
+      <Modal />
+      
     </div>
   )
 }
