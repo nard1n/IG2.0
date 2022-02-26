85934e88ee6677561d40f5c41d9ba6d47615fdb9
Nardin Lachowski <nardin@live.com>
2022-02-26 18:18:41 -0500

    importing recoilroot to represent the store of the atom state within the app

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,11 @@ import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }) {
   return (
-    <RecoilRoot>
-      <SessionProvider session={session}>
+    <SessionProvider session={session}>
+      <RecoilRoot>
         <Component {...pageProps} />
-      </SessionProvider>
-    </RecoilRoot>
+      </RecoilRoot>
+    </SessionProvider>
   )
 }
 
